7ffaeace9c6c2a7e89140678cdb8984eb3a89523
Indrajith <indrajith.bodinayaka352@gmail.com>
2025-03-22 14:14:16 +0530

    create home page and learn something react native

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
+import LandingImage from "@/assets/images/landing.png";
 
-export default function Index() {
+const HomeScreen = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.contatiner}>
+      <Image source={LandingImage} style={styles.image} />
+      <Text>Welcome To App</Text>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  contatiner: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+});
+
+export default HomeScreen;
